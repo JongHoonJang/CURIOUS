1dbf07e8c238d7266b3f996360b491de1af30df9
홍주성 <hjs8482@naver.com>
2022-10-17 15:30:04 +0900

    init

---
+++ back/metroverse/src/main/java/com/ssafy/metroverse/MetroverseApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.metroverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroverseApplication.class, args);
+	}
+
+}
---
+++ back/metroverse/src/test/java/com/ssafy/metroverse/MetroverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.metroverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
