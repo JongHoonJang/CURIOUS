be24edb0ae85ca8caa7dc6153dc7777a5eaff0b4
장종훈 <jangjr98@naver.com>
2022-10-24 09:39:00 +0900

    [FE] feat : front router 설정 및 각 부모 컴포넌트 생성

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,11 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import RandingView from '@/views/randing/RandingView'
+import LoginView from '@/views/randing/LoginView'
+import ProfileView from '@/views/randing/ProfileView'
+import MetaverseView from '@/views/randing/MetaverseView'
+import MainView from '@/views/randing/MainView'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
+  {
+    path: '/',
+    name: 'RandingView',
+    component: RandingView
+  },
+  {
+    path: '/login',
+    name: 'LoginView',
+    component: LoginView
+  },
+  {
+    path: '/profile',
+    name: 'ProfileView',
+    component: ProfileView
+  },
+  {
+    path: '/metaverse',
+    name: 'MetaverseView',
+    component: MetaverseView
+  },
+  {
+    path: '/main',
+    name: 'MainView',
+    component: MainView
+  },
 ]
 
 const router = createRouter({
