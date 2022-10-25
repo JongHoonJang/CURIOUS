3e9e75011d4fe3e770bebbe0af5c17397702369e
pc5401 <pc5401@naver.com>
2022-10-25 14:40:03 +0900

    [FE/MK] 랜딩 페이지 제작 중

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import router from "./router";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router, AOS.init()).mount("#app");
