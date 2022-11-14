f4caba36671d017733e56a312d90cea4883bcff9
에림 <yerim8373@naver.com>
2022-11-14 14:37:45 +0900

    fix : filter 수정

--- back/metroverse/src/main/java/com/ssafy/metroverse/global/filter/JwtAuthenticationFilter.java
+++ back/metroverse/src/main/java/com/ssafy/metroverse/global/filter/JwtAuthenticationFilter.java
@@ -26,26 +26,26 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 		// 헤더에서 JWT 받아옴
 		String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
 		// 유효한 토큰인지 확인
-		if (token != null) {
-			logger.debug("token not null");
-			if (jwtTokenProvider.validateToken(token)) {
-				logger.debug("token ok");
+		if (token != null && jwtTokenProvider.validateToken(token)) {
+			try {
 				// 토큰이 유효하면 토큰으로부터 유저 정보를 받아옴
 				Authentication authentication = jwtTokenProvider.getAuthentication(token);
 				// SecurityContext 에 Authentication 객체를 저장
 				SecurityContextHolder.getContext().setAuthentication(authentication);
-			} else {
-				logger.warn("couldn't find bearer string, will ignore the header");
-				// ((HttpServletResponse)response).sendError(401, "access token 만료");
+			} catch (IllegalArgumentException e) {
+				logger.error("an error occured during getting usesrname from token", e);
 			}
-			// try {
-			//
-			// } catch (IllegalArgumentException e) {
-			// 	logger.error("an error occured during getting usesrname from token", e);
+			// catch (ExpiredJwtException e) { // 유효 기간이 지난 JWT를 수신한 경우
+			// 	logger.warn("the token is expired and not valid anymore", e);
+			// 	((HttpServletResponse)response).sendError(401, "ExpiredJwtException error");
+			// } catch (SignatureException e) { // 서명 오류
+			// 	logger.error("Authentication Failed. Username or Password not valid.");
+			// 	((HttpServletResponse)response).sendError(401, "SignatureException error");
 			// }
-
-			chain.doFilter(request, response);
+		} else {
+			logger.warn("couldn't find bearer string, will ignore the header");
+			//			((HttpServletResponse)response).sendError(401, "access token 만료");
 		}
-
+		chain.doFilter(request, response);
 	}
 }
