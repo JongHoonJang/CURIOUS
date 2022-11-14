4e1145d154d8b6f7e2f5d66c06775f961aae8127
장종훈 <jangjr98@naver.com>
2022-11-14 10:06:22 +0900

    [FE] feat : metaverse 나가기 버튼추가
    Former-commit-id: 7a533619bb2c3c50de01a6ddde65e50a65462bf3

--- front/src/views/metaverse/MetaverseView.vue
+++ front/src/views/metaverse/MetaverseView.vue
@@ -1,9 +1,10 @@
 <template>
-<div class="meta-box">
-  <div style="width: 1000px; height: 600px; border: 1px solid #f00">
-    <canvas id="canvas" ref="canvasRef" style="width: 100%; height: 100%" />
+  <div class="meta-box">
+    <div style="width: 1000px; height: 600px; border: 1px solid #f00">
+      <canvas id="canvas" ref="canvasRef" style="width: 100%; height: 100%" />
+    </div>
   </div>
-</div>
+  <button class="red-btn" @click="close()">나가기</button>
 </template>
 
 <script setup>
@@ -12,7 +13,11 @@ import UnityWebgl from 'unity-webgl';
 
 const canvasRef = ref(null);
 let unityComponent = null;
-
+const close = () => {
+  // self.opener = self;
+  // window.close();
+  window.open('','_self').close(); 
+}
 onMounted(() => {
   if (!unityComponent) {
     unityComponent = new UnityWebgl(canvasRef.value, {
@@ -39,6 +44,27 @@ onBeforeUnmount(() => {
 .meta-box {
   display: flex;
   justify-content: center;
-  margin-top: 100px;
+  margin-top: 50px;
+}
+
+.red-btn {
+  width: 92px;
+  height: 36px;
+  margin-left: 16px;
+  background: radial-gradient(95% 60% at 50% 75%, #d60000 0%, #ff2020 100%);
+  border: 1px solid #fd5454;
+  box-shadow: 0px 8px 20px -8px #ff1111, inset 0px 1px 8px -4px #FFFFFF;
+  border-radius: 12px;
+  color: white;
+  font-size: 16px;
+  line-height: 22px;
+  font-weight: 600;
+  letter-spacing: .02em;
+  transition: all .2s ease;
+  -webkit-tap-highlight-color: rgba(255,255,255,0);
+}
+
+.red-btn:hover {
+  transform: scale(1.2);
 }
 </style>
